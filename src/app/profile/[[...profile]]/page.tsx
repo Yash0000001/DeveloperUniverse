30920ddd72877117203baba3e92d55f84692d5ee
Yash0000001 <yashqwe58@gmail.com>
2024-09-29 13:17:37 +0530

    build error removed

--- src/app/profile/[[...profile]]/page.tsx
+++ src/app/profile/[[...profile]]/page.tsx
@@ -1,22 +1,23 @@
-import { auth, currentUser, User } from '@clerk/nextjs/server'
-import React from 'react'
+import { auth, currentUser } from '@clerk/nextjs/server';
+import React from 'react';
 import { UserProfile } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
 
-const page = async() => {
-    const {userId} = auth();
-    const isAuth = !!userId;
-    const user = await currentUser();
+const page = async () => {
+  const { userId } = auth();  
 
-    if(!auth){
-        redirect("/");
-    }
+  if (!userId) {  
+    redirect("/");
+  }
+
+  const user = await currentUser();  
+
   return (
     <div className='flex items-center justify-center mt-6'>
-        <h1 className='text-2xl'>{user?.username}</h1>
-        <UserProfile/>
+      <h1 className='text-2xl'>{user?.username}</h1>
+      <UserProfile />
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
